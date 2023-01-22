08e00015b80cfdf1610697631169be62d7864e1f
César <114592560+cesarramirezcl@users.noreply.github.com>
2023-01-21 23:39:02 -0300

    now get messages from two subscriptions

--- hybrid/src/main/java/com/vg/gcloud/pubsub/hybrid/GMessageReceiver.java
+++ hybrid/src/main/java/com/vg/gcloud/pubsub/hybrid/GMessageReceiver.java
@@ -8,14 +8,20 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Component;
 
+/*This code defines a class called "GMessageReceiver" that implements the "MessageReceiver" interface from the Google Cloud Pub/Sub library.
+It logs the data of a received PubsubMessage and acknowledges it. It also uses Spring Framework's @Component annotation,
+indicating that the class is a Spring-managed component. This class is expected to be used in a Spring-based application that is
+interacting with Google Cloud Pub/Sub.*/
+
 @Component
 public class GMessageReceiver implements MessageReceiver {
     private static final Log log = LogFactory.getLog(GMessageReceiver.class) ;
 
+
     @Override
     public void receiveMessage(PubsubMessage message, AckReplyConsumer consumer) {
         // TODO Auto-generated method stub
-        log.info(message.getData()) ;
+        log.info(message.getData());
         consumer.ack() ; // this has to be uncommented to acknowledge the message
 
     }
--- hybrid/src/main/java/com/vg/gcloud/pubsub/hybrid/HybridApplication.java
+++ hybrid/src/main/java/com/vg/gcloud/pubsub/hybrid/HybridApplication.java
@@ -11,6 +11,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+/*This code defines a class called "HybridApplication" which is annotated with Spring's @SpringBootApplication annotation,
+indicating that it is a Spring Boot application. It contains a main method which runs the Spring application
+and a CommandLineRunner bean named "getCommandLiner" which is executed when the application runs.
+
+The CommandLineRunner retrieves the default project ID using the ServiceOptions class, sets a constant for the subscription ID to
+"s-invoice", creates a ProjectSubscriptionName object using the project ID and subscription ID, initializes a Subscriber object using
+the subscriptionName, GMessageReceiver and logs the project ID. Then starts the subscriber, awaits for the subscriber to be running and
+terminates it.
+
+This application is a Spring-based application that uses the Google Cloud Pub/Sub library to subscribe to messages on a
+specific subscription in a specific project, and use the GMessageReceiver class to handle the received messages.*/
+
 @SpringBootApplication
 public class HybridApplication {
 
@@ -21,22 +33,36 @@ public class HybridApplication {
 	@Bean
 	public CommandLineRunner getCommandLiner() {
 		return (args) -> {
-			String PROJECT_ID = ServiceOptions.getDefaultProjectId() ;
-			String SUBSCRIPTION_ID = "s-invoice" ;
-			ProjectSubscriptionName subscriptionName = ProjectSubscriptionName.of(PROJECT_ID, SUBSCRIPTION_ID) ;
-			Subscriber subscriber = null ;
-			Log log = LogFactory.getLog(HybridApplication.class) ;
-			log.info(String.format("Project, %s", PROJECT_ID) );
-			try {
-				subscriber = Subscriber.newBuilder(subscriptionName, new GMessageReceiver()).build() ;
-				subscriber.startAsync().awaitRunning();
-				subscriber.awaitTerminated();
-
-			} catch (Exception ex) {
-				ex.printStackTrace();
-			}
+			String PROJECT_ID = ServiceOptions.getDefaultProjectId();
+			String SUBSCRIPTION_ID1 = "s-invoice";
+			String SUBSCRIPTION_ID2 = "s-invoice1_1";
+			ProjectSubscriptionName subscriptionName1 = ProjectSubscriptionName.of(PROJECT_ID, SUBSCRIPTION_ID1);
+			ProjectSubscriptionName subscriptionName2 = ProjectSubscriptionName.of(PROJECT_ID, SUBSCRIPTION_ID2);
+			Log log = LogFactory.getLog(HybridApplication.class);
+			log.info(String.format("Project, %s", PROJECT_ID));
 			
+			Thread thread1 = new Thread(() -> {
+				try {
+					Subscriber subscriber = Subscriber.newBuilder(subscriptionName1, new GMessageReceiver()).build();
+					subscriber.startAsync().awaitRunning();
+					//se puede imprimir hasta aqui
+					subscriber.awaitTerminated();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			});
+			thread1.start();
+
+			Thread thread2 = new Thread(() -> {
+				try {
+					Subscriber subscriber = Subscriber.newBuilder(subscriptionName2, new GMessageReceiver()).build();
+					subscriber.startAsync().awaitRunning();
+					subscriber.awaitTerminated();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			});
+			thread2.start();
 		} ;
 	}
-
 }
